41b82f20bc856c63bc3caf3be1074673361179a4
Yash Nahata <yashnahata16@gmail.com>
2021-06-24 20:23:49 +0530

    Initial commit

---
+++ sort/selectionSort.js
@@ -0,0 +1,37 @@
+async function selectionSort(speed){
+	for(let i = 0; i < arr.length-1; i++){
+		let small = i;
+		for(let j = i+1; j < arr.length; j++){
+			if(arr[j] < arr[small]){
+				small = j;
+			}
+		}
+		bars.children[i].classList.add('current-bar');
+		bars.children[small].classList.add('current-bar');
+		let temp = arr[small];
+		arr[small] = arr[i];
+		arr[i] = temp;
+		let first = bars.children[i].style.height;
+		let second = bars.children[small].style.height;
+		await new Promise((resolve)=>
+			setTimeout(()=>{
+				resolve();
+			},(350 - speed))
+		);
+		bars.children[i].style.height = second;
+		bars.children[small].style.height = first;
+		bars.children[i].classList.remove('current-bar');
+		bars.children[small].classList.remove('current-bar');
+		bars.children[i].classList.add('sorted-bar');
+		bars.children[small].classList.add('sorted-bar');
+		await new Promise((resolve)=>
+			setTimeout(()=>{
+				resolve();
+			},(350 - speed))
+		);
+		bars.children[i].classList.remove('sorted-bar');
+		bars.children[small].classList.remove('sorted-bar');
+	}
+	finalColor();
+	newArrayBtn.disabled = false;
+}
---
+++ sort/sort.js
@@ -0,0 +1,61 @@
+const newArrayBtn = document.getElementById('new-array');
+const startSort = document.getElementById('start-sort');
+const bars = document.getElementById('sorting');
+var arr = [];
+function newArray(){
+	for(let i = 1; i<=100; i++){
+		var random = (Math.floor(Math.random()*100) + 1)*4;
+		arr.push(random);
+		var div = document.createElement('div');
+		div.classList.add('bar');
+		div.style.height = (random + "px");
+		bars.appendChild(div);
+	}
+}
+newArray();
+newArrayBtn.addEventListener('click', ()=>{
+	arr = [];
+	startSort.disabled = false;
+	document.getElementById('sorting').querySelectorAll('*').forEach(n => n.remove());
+	newArray();
+});
+async function finalColor(){
+	for(let i = 0; i < arr.length; i++){
+		bars.children[i].classList.add('sorted-bar');
+		await new Promise((resolve)=>
+			setTimeout(()=>{
+				resolve();
+			},10)
+		);
+	}
+	await new Promise((resolve)=>
+		setTimeout(()=>{
+			resolve();
+		},1500)
+	);
+	for(let i = 0; i < arr.length; i++){
+		bars.children[i].classList.remove('sorted-bar');
+	}
+}
+startSort.addEventListener('click', ()=>{
+	const sortingTechnique = document.getElementById('sorting-technique').value;
+	const speed = document.getElementById('speed').value;
+	newArrayBtn.disabled = true;
+	startSort.disabled = true;
+	if(sortingTechnique === 'bubble-sort'){
+		bubbleSort(speed);
+	}
+	if(sortingTechnique === 'selection-sort'){
+		selectionSort(speed);
+	}
+	if(sortingTechnique === 'insertion-sort'){
+		insertionSort(speed);
+	}
+	if(sortingTechnique === 'merge-sort'){
+		mergeSort(speed);
+	}
+	if(sortingTechnique === 'quick-sort'){
+		quickSort(arr, 0, arr.length-1, speed);
+		console.log(arr);
+	}
+});
