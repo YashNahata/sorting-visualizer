42dcec9201bfe1b88120e35c30dd168018f108df
Yash Nahata <yashnahata16@gmail.com>
2022-02-10 17:42:37 +0530

    Initial Commit

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -1,46 +1,45 @@
-async function bubbleSort(speed){
-	for(let i = 0; i < arr.length-1; i++){
-		for(let j = 0; j < arr.length-i-1; j++){
-			if(arr[j] > arr[j+1]){
-				bars.children[j].classList.add('current-bar');
-				bars.children[j+1].classList.add('current-bar');
-				let temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
-				let first = bars.children[j].style.height;
-				let second = bars.children[j+1].style.height;
-				await new Promise((resolve)=>
-					setTimeout(()=>{
-						resolve();
-					},(350 - speed))
-				);
-				bars.children[j].style.height = second;
-				bars.children[j+1].style.height = first;
-				bars.children[j].classList.remove('current-bar');
-				bars.children[j+1].classList.remove('current-bar');
-				bars.children[j].classList.add('sorted-bar');
-				bars.children[j+1].classList.add('sorted-bar');
-				await new Promise((resolve)=>
-					setTimeout(()=>{
-						resolve();
-					},(350 - speed))
-				);
-				bars.children[j].classList.remove('sorted-bar');
-				bars.children[j+1].classList.remove('sorted-bar');
-			}
-			else{
-				bars.children[j].classList.add('sorted-bar');
-				bars.children[j+1].classList.add('sorted-bar');
-				await new Promise((resolve)=>
-					setTimeout(()=>{
-						resolve();
-					},(350 - speed))
-				);
-				bars.children[j].classList.remove('sorted-bar');
-				bars.children[j+1].classList.remove('sorted-bar');
-			}
-		}
-	}
-	finalColor();
-	newArrayBtn.disabled = false;
+async function bubbleSort(speed) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        bars.children[j].classList.add("current-bar");
+        bars.children[j + 1].classList.add("current-bar");
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        let first = bars.children[j].style.height;
+        let second = bars.children[j + 1].style.height;
+        await new Promise((resolve) =>
+          setTimeout(() => {
+            resolve();
+          }, 350 - speed)
+        );
+        bars.children[j].style.height = second;
+        bars.children[j + 1].style.height = first;
+        bars.children[j].classList.remove("current-bar");
+        bars.children[j + 1].classList.remove("current-bar");
+        bars.children[j].classList.add("sorted-bar");
+        bars.children[j + 1].classList.add("sorted-bar");
+        await new Promise((resolve) =>
+          setTimeout(() => {
+            resolve();
+          }, 350 - speed)
+        );
+        bars.children[j].classList.remove("sorted-bar");
+        bars.children[j + 1].classList.remove("sorted-bar");
+      } else {
+        bars.children[j].classList.add("sorted-bar");
+        bars.children[j + 1].classList.add("sorted-bar");
+        await new Promise((resolve) =>
+          setTimeout(() => {
+            resolve();
+          }, 350 - speed)
+        );
+        bars.children[j].classList.remove("sorted-bar");
+        bars.children[j + 1].classList.remove("sorted-bar");
+      }
+    }
+  }
+  finalColor();
+  newArrayBtn.disabled = false;
 }
--- sort/sort.js
+++ sort/sort.js
@@ -1,51 +1,54 @@
-const newArrayBtn = document.getElementById('new-array');
-const startSort = document.getElementById('start-sort');
-const bars = document.getElementById('sorting');
+const newArrayBtn = document.getElementById("new-array");
+const startSort = document.getElementById("start-sort");
+const bars = document.getElementById("sorting");
 var arr = [];
-function newArray(){
-	for(let i = 1; i<=100; i++){
-		var random = (Math.floor(Math.random()*100) + 1)*4;
-		arr.push(random);
-		var div = document.createElement('div');
-		div.classList.add('bar');
-		div.style.height = (random + "px");
-		bars.appendChild(div);
-	}
+function newArray() {
+  for (let i = 1; i <= 100; i++) {
+    var random = (Math.floor(Math.random() * 100) + 1) * 4;
+    arr.push(random);
+    var div = document.createElement("div");
+    div.classList.add("bar");
+    div.style.height = random + "px";
+    bars.appendChild(div);
+  }
 }
 newArray();
-newArrayBtn.addEventListener('click', ()=>{
-	arr = [];
-	startSort.disabled = false;
-	document.getElementById('sorting').querySelectorAll('*').forEach(n => n.remove());
-	newArray();
+newArrayBtn.addEventListener("click", () => {
+  arr = [];
+  startSort.disabled = false;
+  document
+    .getElementById("sorting")
+    .querySelectorAll("*")
+    .forEach((n) => n.remove());
+  newArray();
 });
-async function finalColor(){
-	for(let i = 0; i < arr.length; i++){
-		bars.children[i].classList.add('sorted-bar');
-		await new Promise((resolve)=>
-			setTimeout(()=>{
-				resolve();
-			},10)
-		);
-	}
-	await new Promise((resolve)=>
-		setTimeout(()=>{
-			resolve();
-		},1500)
-	);
-	for(let i = 0; i < arr.length; i++){
-		bars.children[i].classList.remove('sorted-bar');
-	}
+async function finalColor() {
+  for (let i = 0; i < arr.length; i++) {
+    bars.children[i].classList.add("sorted-bar");
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        resolve();
+      }, 10)
+    );
+  }
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      resolve();
+    }, 1500)
+  );
+  for (let i = 0; i < arr.length; i++) {
+    bars.children[i].classList.remove("sorted-bar");
+  }
 }
-startSort.addEventListener('click', ()=>{
-	const sortingTechnique = document.getElementById('sorting-technique').value;
-	const speed = document.getElementById('speed').value;
-	newArrayBtn.disabled = true;
-	startSort.disabled = true;
-	if(sortingTechnique === 'bubble-sort'){
-		bubbleSort(speed);
-	}
-	if(sortingTechnique === 'selection-sort'){
-		selectionSort(speed);
-	}
+startSort.addEventListener("click", () => {
+  const sortingTechnique = document.getElementById("sorting-technique").value;
+  const speed = document.getElementById("speed").value;
+  newArrayBtn.disabled = true;
+  startSort.disabled = true;
+  if (sortingTechnique === "bubble-sort") {
+    bubbleSort(speed);
+  }
+  if (sortingTechnique === "selection-sort") {
+    selectionSort(speed);
+  }
 });
